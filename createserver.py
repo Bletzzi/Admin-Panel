599f8c4f49e9f271a8ead43b5bef72f2975dd0ab
Bletzzi <90057273+Bletzzi@users.noreply.github.com>
2022-03-31 19:02:40 -0300

    Create createserver.py

---
+++ createserver.py
@@ -0,0 +1,21 @@
+#AdminPanel for Minecraft
+import sys
+import os
+import subprocess
+import time
+import json
+import createserver
+
+def createSpigot():
+    print("Creating Spigot Server...")
+    time.sleep(1)
+    #create a linux screen
+    subprocess.call(["screen", "-dmS", "spigot"])
+    #install spigot egg
+    subprocess.call(["screen", "-S", "spigot", "-X", "stuff", "cd /home/minecraft/minecraft-server/ && wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar && java -jar BuildTools.jar --rev 1.12.2-R0.1-SNAPSHOT && rm BuildTools.jar && wget https://hub.spigotmc.org/jenkins/job/Spigot/lastSuccessfulBuild/artifact/spigot-1.12.2-R0.1-SNAPSHOT.jar && mv spigot-1.12.2-R0.1-SNAPSHOT.jar spigot.jar && screen -S spigot -X stuff 'java -Xmx1024M -Xms1024M -jar spigot.jar nogui'\n"])
+    #start the server
+    subprocess.call(["screen", "-S", "spigot", "-X", "stuff", "java -Xmx1024M -Xms1024M -jar spigot.jar nogui\n"])
+    #wait for the server to start
+    time.sleep(5)
+    #check if the server is running
+    subprocess.call(["screen", "-S", "spigot", "-X", "stuff", "screen -S spigot -X stuff 'echo \"Server is running!\"\n'"])
